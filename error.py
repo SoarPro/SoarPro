80ebe553334ae7faa53a4353d583ed95a50b3c32
Oluwaseyifunmi Oyefeso <gistkiosk@gmail.com>
2021-03-19 18:41:44 +0100

    Added errors

--- error.py
+++ error.py
@@ -12,6 +12,7 @@ Error on line 1 index {self.pos}:
 class BadCharacterError(Error):
     def __init__(self, pos, char):
         super().__init__(pos, f"Found a mystery character: {char!r}")
+        
 class FloatError(Error):
     def __init__(self, pos):
         super().__init__(pos, f"Invalid float! Only one '.' is allowed in a float")
