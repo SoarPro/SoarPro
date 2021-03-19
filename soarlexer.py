f6c51a578595058aa2f3e6bfb8755ca09160b632
sheyziSilver <sheyzilife.info@gmail.com>
2021-03-19 07:55:04 -0700

    Added some comments

---
+++ soarlexer.py
@@ -0,0 +1,9 @@
+"""
+This is the very first version of the SoarPro Lexer.
+It takes input from the user and then returns the appropriate 
+result to the user.
+"""
+
+####################################################
+#################### Import ########################
+####################################################
---
+++ soarparser.py
@@ -0,0 +1,9 @@
+"""
+This is the very first version of the SoarPro Parser.
+It takes input from the user and then returns the appropriate 
+result to the user.
+"""
+
+####################################################
+#################### Import ########################
+####################################################
