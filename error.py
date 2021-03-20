d71cb9c0bc129ba4461df9e3595ff9e2400549ca
sheyziSilver <sheyzilife.info@gmail.com>
2021-03-20 07:49:48 -0700

    Added a much cleaner error message

--- error.py
+++ error.py
@@ -1,18 +1,19 @@
 class Error:
-    def __init__(self, pos, details):
+    def __init__(self, pos, error_name, details):
         self.pos = pos
+        self.error_name = error_name
         self.details = details
 
     def as_string(self):
-        return f"""
-Error on line {self.pos.lineno} index {self.pos.column}:
-  {self.details}
-        """
+        result = f"{self.error_name}: {self.details}\n"
+        result += f"File stdin, line {self.pos.lineno}, index {self.pos.column}"
+        return result
+        
 
 class BadCharacterError(Error):
     def __init__(self, pos, char):
-        super().__init__(pos, f"Found a mystery character: {char!r}")
+        super().__init__(pos, "Bad Character error", {char})
         
 class FloatError(Error):
     def __init__(self, pos):
-        super().__init__(pos, f"Invalid float! Only one '.' is allowed in a float")
+        super().__init__(pos, "Invalid float", "Only one '.' is allowed")
