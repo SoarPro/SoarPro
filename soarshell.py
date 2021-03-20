77587a15f02b06c5448f00bb4586b4c839e3d55f
sheyziSilver <sheyzilife.info@gmail.com>
2021-03-19 17:04:21 -0700

    Updated the shell

--- soarshell.py
+++ soarshell.py
@@ -35,7 +35,7 @@ if len(sys.argv) == 2:
             print(f"SoarPro can't open {data!r}")
 
 else:
-    print("Welcome to XLang Version 1.0.0")
+    print("Welcome to SoarPro Version 1.0.0")
     print("For more informations type 'help', 'about' or 'leave' to exit this shell")
 
     while True:
@@ -44,9 +44,9 @@ else:
         # This happens whenever the user inputs "help" without the quotef
         if text == "help":
             help = """
-    Welcome to the Xlang shell!
-    If you are new to using Xlang you can checkout the tutorial
-    on github at https://github.com.ng/XBid/XLang.
+    Welcome to the SoarPro shell!
+    If you are new to using SoarPro you can checkout the tutorial
+    on github at https://github.com/SoarPro/SoarPro.
 
     This shell is meant to be used for testing purposes only. If you
     need to use it for more complex stuff check out the compiler! 
@@ -56,7 +56,7 @@ else:
         # This happens when the user inputs "about without quotes"
         elif text == "about":
             about = """
-    XLang is a compiled, high level and general purpose programming
+    SoarPro is a compiled, high level and general purpose programming
     language. The first intention of building this programming language
     was to learn how it is been built we at XBid hopes some day it meets
     production standard!!
@@ -65,7 +65,7 @@ else:
 
         # This happens when the user inputs "leave" it also stops the shell from running
         elif text == "leave":
-            print("Thanks for using XLang... Have a nice day!")
+            print("Thanks for using SoarPro... Have a nice day!")
             break
 
         # This happens if the user inputs a normal code. e.g: x = 32;
